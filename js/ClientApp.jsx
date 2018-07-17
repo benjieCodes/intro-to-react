99868e012abf3894366ec4e38ef192fc786e2df8
Benjie <benjiecodes@gmail.com>
2018-07-17 16:37:36 -0400

    all tools have been added - LETS GET STARTED

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,24 +1,2 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-const MyTitle = function(props) {
-  // return ce('div',null,ce('h1', { style: { color: props.color } }, props.title));
-  return (
-    <div>
-      <h1 style={{ color: props.color }}>{props.title}</h1>
-    </div>
-  );
-};
-
-const MyFirstComponent = function() {
-  return (
-    <div id="my-first-component">
-      <MyTitle title="Game of Thrones" color="YellowGreen" />
-      <MyTitle title="Stranger Things" color="GreenYellow" />
-      <MyTitle title="Rick and Morty" color="LimeGreen" />
-      <MyTitle title="Silicon Valley" color="Peru" />
-    </div>
-  );
-};
-
-render(<MyFirstComponent />, document.getElementById('app'));
