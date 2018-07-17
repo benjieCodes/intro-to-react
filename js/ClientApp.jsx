5875dbab09267f9fdd7df7434d1521018d76b025
Benjie <benjiecodes@gmail.com>
2018-07-17 16:58:55 -0400

    added initial search ui for app

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>S Video</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
+
+render(<App />, document.getElementById('app'));
