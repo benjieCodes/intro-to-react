df84eaa6b236ac7350a52bbd5a8f337e6c730e0a
Benjie <benjiecodes@gmail.com>
2018-07-19 11:15:50 -0400

    using BrowserRouter instead of HashRouter and added api fallback to go to index.html

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowswerRouter, Route } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 
 const App = () => (
-  <HashRouter>
+  <BrowswerRouter>
     <div className="app">
       <Route exact path="/" component={Landing} />
       <Route path="/search" component={Search} />
     </div>
-  </HashRouter>
+  </BrowswerRouter>
 );
 
 render(<App />, document.getElementById('app'));
