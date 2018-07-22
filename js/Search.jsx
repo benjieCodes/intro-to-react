8a4941186453f6daa51843530f357be17ab97b97
benjiecodes <benjiesongsong@Benjies-MacBook-Pro.local>
2018-07-21 21:27:46 -0400

    adding state to Search component with an onchange function

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const Search = () => (
-  <div className="search">
-    <header>
-      <h1>Intro to React - S-Video</h1>
-      <input type="text" placeholder="Search" />
-    </header>
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: 'dsf'
+    };
+  }
 
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>Intro to React - S-Video</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
