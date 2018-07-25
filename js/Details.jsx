000a8856cce02ababbf31a353dc4edd375faebb4
benjiecodes <benjiesongsong@Benjies-MacBook-Pro.local>
2018-07-25 13:17:59 -0400

    added the details component

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { string } from 'prop-types';
 
-const Details = props => (
+const Details = props => {
+  const { title, description, year, poster, trailer } = props.show;
+  return (
     <div className="details">
-      <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
+      <header>
+        <h1>S-Video</h1>
+      </header>
+      <section>
+        <h1>{title}</h1>
+        <h2>{year}</h2>
+        <img
+          src={`/public/img/posters/${poster}`}
+          alt={`Poster for ${title}`}
+        />
+        <p>{description}</p>
+      </section>
+      <div>
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+          frameBorder="0"
+          allowFullScreen
+          title={`Trailer for ${title}`}
+        />
+      </div>
     </div>
   );
+};
+
+Details.propTypes = {
+  show: string.isRequired
+};
 
 export default Details;
