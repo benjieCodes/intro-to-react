72046c6b61926f330c075e653afa9f7412943fcc
benjiecodes <benjiecodes@gmail.com>
2018-07-30 12:11:59 -0400

    added an async return in redux

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { SET_SEARCH_TERM, ADD_API_DATA } from './actions';
 
 export function setSearchTerm(searchTerm) {
@@ -6,4 +7,17 @@ export function setSearchTerm(searchTerm) {
 
 export function addAPIData(apiData) {
   return { type: ADD_API_DATA, payload: apiData };
+}
+
+export function getAPIDetails(imdbID) {
+  return dispatch => {
+    axios
+      .get(`http://localhost:3000/${imdbID}`)
+      .then(response => {
+        dispatch(addAPIData(response.data));
+      })
+      .catch(error => {
+        console.error('axios error', error); // eslint-disable-line no-console
+      });
+  };
 }
