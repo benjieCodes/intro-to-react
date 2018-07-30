41afe5a8a67a58b8828f4839d6e89ac5ef548cb8
benjiecodes <benjiecodes@gmail.com>
2018-07-30 14:42:19 -0400

    testing using redux

--- js/__tests__/Search.test.jsx
+++ js/__tests__/Search.test.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import store from '../store';
+import { setSearchTerm } from '../actionCreators';
 import preload from '../../data.json';
 import Search, { Unwrapped as UnwrappedSearch } from '../Search';
 import ShowCard from '../ShowCard';
@@ -20,8 +24,13 @@ test('Search should render correct amount of shows', () => {
 
 test('Search should render correct amount of shows based on search term', () => {
   const searchWord = 'black';
-  const component = shallow(
-    <UnwrappedSearch shows={preload.shows} searchTerm={searchWord} />
+  store.dispatch(setSearchTerm(searchWord));
+  const component = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Search shows={preload.shows} searchTerm={searchWord} />
+      </MemoryRouter>
+    </Provider>
   );
   // component
   //   .find('input')
@@ -33,5 +42,5 @@ test('Search should render correct amount of shows based on search term', () => 
         .toUpperCase()
         .indexOf(searchWord.toUpperCase()) >= 0
   ).length;
-  expect(component.find(ShowCard).length).toEqual(showCount);
+  expect(component.find('.show-card').length).toEqual(showCount);
 });
