8808d3e39d18e8f3ebb4a04c4c1591791bd9c660
benjiecodes <benjiecodes@gmail.com>
2018-07-30 16:51:40 -0400

    adding server side rendering

--- js/App.jsx
+++ js/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { string } from 'prop-types';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,29 +11,27 @@ import preload from '../data.json';
 const FourOhFour = () => <h1>404 Page not found.</h1>;
 
 const App = () => (
-  <BrowserRouter>
-    <Provider store={store}>
-      <div className="app">
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route
-            path="/search"
-            component={props => <Search shows={preload.shows} {...props} />}
-          />
-          <Route
-            path="/details/:id"
-            component={props => {
-              const selectedShow = preload.shows.find(
-                show => props.match.params.id === show.imdbID
-              );
-              return <Details show={selectedShow} {...props} />;
-            }}
-          />
-          <Route component={FourOhFour} />
-        </Switch>
-      </div>
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route
+          path="/search"
+          component={props => <Search shows={preload.shows} {...props} />}
+        />
+        <Route
+          path="/details/:id"
+          component={props => {
+            const selectedShow = preload.shows.find(
+              show => props.match.params.id === show.imdbID
+            );
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </Provider>
 );
 
 App.propTypes = {
